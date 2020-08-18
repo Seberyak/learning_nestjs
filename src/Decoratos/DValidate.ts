b21df23e87146888eb188ff3d95bb23fda9045bc
Seberyak <v_liparteliani@cu.edu.ge>
2020-08-18 13:17:02 +0400

    DValidate.ts Fixed.

--- src/Decoratos/DValidate.ts
+++ src/Decoratos/DValidate.ts
@@ -4,15 +4,16 @@ import { IAPOSTUserSchema } from '../users/Types/IAPOSTUserSchema';
 
 
 export const DValidate = createParamDecorator((schema, ctx:ExecutionContext)=>{
-  const data:IAPOSTUserSchema = {...ctx.switchToHttp().getRequest().body, ...ctx.switchToHttp().getRequest().query, ...ctx.switchToHttp().getRequest().params};
-  const  {value, error} = schema.validate(data);
-  console.log(value);
 
-  return  !error? value: validationError();
-  // try {
-  // }
-  // catch (error){
-  //   validationError()
-  // }
+
+
+  try {
+    const data:IAPOSTUserSchema = {...ctx.switchToHttp().getRequest().body, ...ctx.switchToHttp().getRequest().query, ...ctx.switchToHttp().getRequest().params};
+    const  {value, error} = schema.validate(data);
+    return  !error? value: validationError();
+  }
+  catch (error){
+    validationError()
+  }
 
 })
